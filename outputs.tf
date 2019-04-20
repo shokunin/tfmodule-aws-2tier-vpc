d4c778051fc608c093ebc2e61e205cc2119b938c
Chris Mague <github@mague.com>
2019-04-20 11:37:42 -0700

    add more output information

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,18 @@ output "vpc-id" {
   value = "${aws_vpc.vpc.id}"
 }
 
+output "vpc-cidr" {
+  value = "${var.vpc-cidr}"
+}
+output "subnets-public" {
+  value = ["${aws_subnet.public.*.id}"]
+}
+output "subnets-private" {
+  value = ["${aws_subnet.private.*.id}"]
+}
+output "vpc-azs" {
+  value = "${var.vpc-azs}"
+}
+output "private-nats" {
+  value = ["${aws_eip.private-nat-eip.*.public_ip}"]
+}
